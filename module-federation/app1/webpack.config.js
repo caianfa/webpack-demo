b9a8b6e7104f6075f6b213c3f5106141e7dfc63d
caianfa <caianfa@meituan.com>
2021-12-02 15:29:59 +0800

    d

--- module-federation/app1/webpack.config.js
+++ module-federation/app1/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {ModuleFederationPlugin} = require("webpack").container;
+const { ModuleFederationPlugin } = require("webpack").container;
 const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
 const path = require("path");
 
@@ -32,7 +32,7 @@ module.exports = {
       remotes: {
         app2: "app2@[app2Url]/remoteEntry.js",
       },
-      shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
     }),
     new ExternalTemplateRemotesPlugin(),
     new HtmlWebpackPlugin({
